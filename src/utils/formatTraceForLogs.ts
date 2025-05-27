04214e9337ee9a26531b711d453d9a0f532d5b9c
Aime Comeault <robinhoodblock71@gmail.com>
2025-05-27 00:41:17 -0600

    Create formatTraceForLogs.ts

---
+++ src/utils/formatTraceForLogs.ts
@@ -0,0 +1,21 @@
+// src/utils/formatTraceForLogs.ts
+
+import { TraceResult } from "@/types/traceTypes";
+
+export function formatTraceForLogs(trace: TraceResult, indent = 0): string {
+  const prefix = " ".repeat(indent * 2);
+  let output = `${prefix}- Method: ${trace.method}\n`;
+  output += `${prefix}  Contract: ${trace.contract}\n`;
+  output += `${prefix}  Args: ${JSON.stringify(trace.args)}\n`;
+  output += `${prefix}  ETH Sent: ${trace.ethTransferred}\n`;
+  output += `${prefix}  Gas Used: ${trace.gasUsed}\n`;
+
+  if (trace.children.length) {
+    output += `${prefix}  Subcalls:\n`;
+    for (const child of trace.children) {
+      output += formatTraceForLogs(child, indent + 1);
+    }
+  }
+
+  return output;
+}
